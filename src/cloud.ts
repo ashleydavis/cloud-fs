cb7607fb52394ae319296c646a3c455320bdfb12
Ashley Davis <ashley@codecapers.com.au>
2021-02-11 14:59:41 +1000

    Now wrapping Vorpal actions in functions to handle errors. Vorpal doesn't seem to handle rejected promises.

--- src/cloud.ts
+++ src/cloud.ts
@@ -3,27 +3,53 @@ import { CloudFS } from ".";
 const app =  new Vorpal();
 const cloudFS = new CloudFS();
 
+type VorpalFn = (args: Vorpal.Args) => Promise<void>;
+
+//
+// Wrap functions because Vorpal doesn't appear to handle rejected promies.
+//
+function wrapAction(fn: VorpalFn): VorpalFn {
+    return async (args: Vorpal.Args) => {
+        try {
+            await fn(args);
+        }
+        catch (err) {
+            console.error(`Failed with error:`);
+            if (err.stack) {
+                console.error(err.stack);
+            }
+            else if (err.message) {
+                console.error(err.message);
+            }
+            else {
+                console.error(err);
+            }
+            process.exit(1);
+        }
+    }
+}
+
 app
     .command("pwd", "Prints the current working directory.")
-    .action(async args => {
+    .action(wrapAction(async args => {
         console.log(cloudFS.pwd());
-    });
+    }));
 
 app
     .command("cd <dir>", "Changes the working directory.")
     // .example("cd /", "Change to the root directory.")
     // .example("cd /aws", "Change to the root directory for AWS storage.")
     // .example("cd /az", "Change to the root directory for Azure storage.")
-    .action(async args => {
+    .action(wrapAction(async args => {
         cloudFS.cd(args.dir);
-    });
+    }));
     
 app
     .command("ls [dir]", "Lists files and directories.")
     .option("-l, --long", "Enables long listing format")
     // .example("ls subdir", "Lists files and directories under 'subdir'.")
     // .example("ls aws:subdir", "Lists files and directories in AWS under 'subdir'.")
-    .action(async args => {
+    .action(wrapAction(async args => {
         const nodes = cloudFS.ls(args.dir && args.dir.trim());
         if (args.long) {
             for await (const node of nodes) {
@@ -45,16 +71,16 @@ app
             }
             process.stdout.write("\n");
         }
-    });
+    }));
 
 app
     .command("cp <src> <dest>", "Copies files and directories.")
     // .example("cp somefile.txt someotherfile.txt", "Copy a file from one place to another.")
     // .example("cp somefile.txt somedirectory/", "Copy a file to an output directory.")
     // .example("cp somedirectory/ someotherdirectory/", "Copies all files in some directory to another directory.")
-    .action(async args => {
+    .action(wrapAction(async args => {
         await cloudFS.cp(args.src.trim(), args.dest.trim());
-    });
+    }));
 
 if (process.argv.length === 2) {
     app
